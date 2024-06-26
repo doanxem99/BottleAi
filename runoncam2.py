98aebc2a8997ebe64a1525957a60a47c6072bc8b
doanxem99 <doanxem99@LAPTOP-31R959C9>
2024-06-26 18:06:48 +0700

    Add code

---
+++ runoncam2.py
@@ -0,0 +1,35 @@
+import cv2
+from picamera2 import Picamera2
+
+from ultralytics import YOLO
+
+# Initialize the Picamera2
+picam2 = Picamera2()
+picam2.preview_configuration.main.size = (1280, 720)
+picam2.preview_configuration.main.format = "RGB888"
+picam2.preview_configuration.align()
+picam2.configure("preview")
+picam2.start()
+
+# Load the YOLOv8 model
+model = YOLO("yolov8n.pt")
+
+while True:
+    # Capture frame-by-frame
+    frame = picam2.capture_array()
+
+    # Run YOLOv8 inference on the frame
+    results = model(frame)
+
+    # Visualize the results on the frame
+    annotated_frame = results[0].plot()
+
+    # Display the resulting frame
+    cv2.imshow("Camera", annotated_frame)
+
+    # Break the loop if 'q' is pressed
+    if cv2.waitKey(1) == ord("q"):
+        break
+
+# Release resources and close windows
+cv2.destroyAllWindows()
