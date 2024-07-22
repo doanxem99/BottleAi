fb5b37139aa98b030ff49673fc4c4374c4305485
Dinh Manh <doanxem99@LAPTOP-31R959C9.>
2024-07-22 17:48:20 +0700

    ncnn_model 200 image dataset

--- best_ncnn_model/model_ncnn.py
+++ best_ncnn_model/model_ncnn.py
@@ -8,8 +8,8 @@ def test_inference():
     out = []
 
     with ncnn.Net() as net:
-        net.load_param("best_ncnn_model/model.ncnn.param")
-        net.load_model("best_ncnn_model/model.ncnn.bin")
+        net.load_param("best_2_ncnn_model/model.ncnn.param")
+        net.load_model("best_2_ncnn_model/model.ncnn.bin")
 
         with net.create_extractor() as ex:
             ex.input("in0", ncnn.Mat(in0.squeeze(0).numpy()).clone())
